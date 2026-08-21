e42366693fa14f491e24920ed40193b40da83b8e
nguyentthai96 <nguyentthai96@gmail.com>
2026-08-21 16:04:21 +0700

    chore(auth-service): update main application file

--- src/main/kotlin/com/ntt/authservice/AuthServiceApplication.kt
+++ src/main/kotlin/com/ntt/authservice/AuthServiceApplication.kt
@@ -1,9 +1,12 @@
 package com.ntt.authservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import com.ntt.basecore.autoconfigure.data.DataProperties
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataProperties::class)
 class AuthServiceApplication
 
 fun main(args: Array<String>) {
